10f7d820e7c186fe8dccb700250515952ee29049
grilledwindow <xaveang@gmail.com>
2022-10-13 00:36:05 +0800

    Add quoteProps to prettier config; Format tailwind config

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -6,17 +6,17 @@ module.exports = {
 			colors: {
 				'pine-glade': '#C7D19E',
 				'ship-cove': '#6E91B9',
-				mirage: {
+				'mirage': {
 					DEFAULT: '#191F2D',
 					darker: '#151B29'
 				},
 				'cadet-blue': '#A0A8C3',
-				comet: {
+				'comet': {
 					DEFAULT: '#636985',
 					darker: '#434965'
 				},
 				'orange-red-crayola': '#FF5A5F',
-				cultured: '#F5F5F5',
+				'cultured': '#F5F5F5',
 				'tea-green': '#C4D6B0'
 			},
 			fontFamily: {
