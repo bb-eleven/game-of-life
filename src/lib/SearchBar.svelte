fbdf95d111d76244d208e8530b86ff89cca34fec
grilledwindow <xaveang@gmail.com>
2022-10-25 00:43:10 +0800

    Remove spellcheck in search bar

--- src/lib/SearchBar.svelte
+++ src/lib/SearchBar.svelte
@@ -6,6 +6,7 @@
 	<input
 		type="text"
 		placeholder="Search"
+		spellcheck="false"
 		bind:value={search}
 		class="bg-mirage placeholder:text-comet-darker text-comet select-none rounded-md border-2 border-comet p-1 pl-2 active:outline-none focus:outline-none"
 	/>
