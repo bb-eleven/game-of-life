1c9293cb6eedf75dc82a04290cb6309a8c15eac0
grilledwindow <xaveang@gmail.com>
2022-10-24 20:13:11 +0800

    Update search bar style

--- src/lib/SearchBar.svelte
+++ src/lib/SearchBar.svelte
@@ -3,5 +3,10 @@
 </script>
 
 <label>
-	<input type="text" bind:value={search} />
+	<input
+		type="text"
+		placeholder="Search"
+		bind:value={search}
+		class="bg-mirage placeholder:text-comet-darker text-comet select-none rounded-md border-2 border-comet p-1 pl-2 active:outline-none focus:outline-none"
+	/>
 </label>
